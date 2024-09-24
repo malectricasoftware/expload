06c11b96b6656d5be6667d417b7dd5fa69bfc147
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2024-09-24 20:34:54 +0100

    Update parse.py

--- exploadlib/parse.py
+++ exploadlib/parse.py
@@ -6,7 +6,17 @@ def parser():
 	parser.add_argument("-e", "--ext",required=True,help="extension to spoof")
 	parser.add_argument("-n", "--name",required=True,help="field name for file upload")
 	parser.add_argument("-f", "--filename",required=True,help="file name to upload with")
+	parser.add_argument("-d", "--doubleextend",action="store_true",help="spoofed extension inserted into filename")
 	parser.add_argument("-h2", "--http2",action="store_true",help="use http2 if supported")
+	parser.add_argument("-he", "--headers",help="headers and keys colon seperated",nargs="+")
+	parser.add_argument("-c", "--cookies",help="cookies seperated by ; and wrapped in quotes")
+	parser.add_argument("-r", "--response",action="store_true",help="display the response from the target webapp")
+	
 
 	args = parser.parse_args()
+	if args.headers:
+		args.headers={header.split(":")[0]:header.split(":")[1] for header in args.headers}
+	if args.cookies:
+            args.headers["cookie"]=args.cookies
+
 	return args
